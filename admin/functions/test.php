1ebe26ba13953ecba7e9b24f1f90d9edba4a667b
William Kimambo <jamesjojo461@gmail.com>
2024-05-23 21:01:53 +0200

    testind test.php conn

--- admin/functions/test.php
+++ admin/functions/test.php
@@ -1,15 +1,4 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "invento_db";
-// // Create a connection
-// $conn = new mysqli($servername, $username, $password, $database);
-
-// // Check the connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
 
 // Initialize MySQLi
 $conn = mysqli_init();
@@ -18,9 +7,9 @@ if (!$conn) {
 }
 
 // Set up SSL for the connection
-if (!mysqli_ssl_set($conn, NULL, NULL, "../../assets/Microsoft RSA Root Certificate Authority 2017.crt", NULL, NULL)) {
-    die('MySQLi SSL setup failed: ' . mysqli_error($conn));
-}
+// if (!mysqli_ssl_set($conn, NULL, NULL, "../../assets/Microsoft RSA Root Certificate Authority 2017.crt", NULL, NULL)) {
+//     die('MySQLi SSL setup failed: ' . mysqli_error($conn));
+// }
  
 // Attempt to connect to the database
 if (!mysqli_real_connect($conn, "william.mysql.database.azure.com", "williamUser", "Writer@becket#360", "invento_db", 3306, NULL, MYSQLI_CLIENT_SSL)) {
