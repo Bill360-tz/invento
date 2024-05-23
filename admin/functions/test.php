07308a73a749823c620e7450484cb308fb4e60a1
William Kimambo <jamesjojo461@gmail.com>
2024-05-23 20:48:43 +0200

    testing the test.php connection file

--- admin/functions/test.php
+++ admin/functions/test.php
@@ -1,11 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Php</title>
-</head>
-<body>
-    <h1>Test New File</h1>
-</body>
-</html>
+<?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "invento_db";
+// // Create a connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// // Check the connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// Initialize MySQLi
+$conn = mysqli_init();
+if (!$conn) {
+    die('MySQLi initialization failed');
+}
+
+// Set up SSL for the connection
+if (!mysqli_ssl_set($conn, NULL, NULL, "../../assets/Microsoft RSA Root Certificate Authority 2017.crt", NULL, NULL)) {
+    die('MySQLi SSL setup failed: ' . mysqli_error($conn));
+}
+ 
+// Attempt to connect to the database
+if (!mysqli_real_connect($conn, "william.mysql.database.azure.com", "williamUser", "Writer@becket#360", "invento_db", 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+// Connection successful
+// echo 'Success... ' . mysqli_get_host_info($conn) . "\n";
+
+// You can now use $conn for your queries
+
+// Close the connection when done
+?>
+
