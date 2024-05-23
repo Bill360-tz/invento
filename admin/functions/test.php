a1c7f4aff5e5a33c3144df5cc898b17b5f53699c
William Kimambo <jamesjojo461@gmail.com>
2024-05-23 14:14:18 +0200

    File exist test

---
+++ admin/functions/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Php</title>
+</head>
+<body>
+    <h1>Test New File</h1>
+</body>
+</html>
